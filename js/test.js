ff20e1b5beb5eb1a195ede0a1e547d30a0bd0716
Yusuf <demiryusuf1918@gmail.com>
2026-04-02 20:28:49 +0300

    dosyalar tekrar değişti dosyalar tekrar değişti

--- js/test.js
+++ js/test.js
@@ -8,7 +8,7 @@
     <h1>MERHABA BENIM TATLI KIZIM</h1>
     <p>EGER INTERNETTE BIR SEY ARASTIRMAK ISTERSEN ASAGIDAKI LINKE TIKLAYABILIRSIN</p>
     <p>BUNU KULLANIRKEN DIKKATLI OLMAILISIN</p>
-     <a href="https://www.google.com">Google'a gIt</a>
+     <a href="https://www.google.com">Google'a gıt</a>
     <p>AYRICA MAVI ZAMBAK MAGAZA LINKI DE BURADA SATIN ALMAK ISTEDIGIN SEYLER ICIN BURAYA TIKLA</p>
     <a href="https://www.hepsiburada.com/magaza/mavi-zambak">mavi zambak</a>
     <p>TRENDYOLDA BIR SEY ALMAK ISTERSEN DE BU LINKE TIKLAYABILIRSIN</p>
